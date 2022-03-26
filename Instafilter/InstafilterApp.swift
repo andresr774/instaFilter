cdb7e086d9d40a01c9f79b4f7abee1f49f75a21f
Andres <andresr774@gmail.com>
2022-03-26 18:29:41 -0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Andres camilo Raigoza misas on 26/03/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
